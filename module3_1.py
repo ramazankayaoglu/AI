44475a2caeb043c8af272c5835d7b7e92690b110
Ramazan Kayaoğlu <ramazan.kayaoglu@outlook.com>
2026-05-26 18:40:56 +0300

    lesson 28 finished

--- module3_1.py
+++ module3_1.py
@@ -44,16 +44,42 @@ q_tokens2 = q_tokenizer.tokenize(prompt)
 #print(q_tokens2)
 
 q_out = q_model(torch.tensor([q_tokens]))
-#print(q_out)
 
 
 #print(q_out.logits.shape)
 
-last_token = q_out.logits[0, -1, :]
+last_token = q_out.logits[0, 3, :]
 
 probs = torch.softmax(last_token, dim = -1)
 max_prob, max_index = torch.max(probs, dim = -1)
 
-print(max_prob, max_index)
+#print(max_prob, max_index)
+#print(q_tokenizer.decode(max_index))
 
-print(q_tokenizer.decode(max_index))
+
+#print(q_tokens)
+#input = [1782, 6722, 315, 28192] "the capital of united"
+#output = [38297, 315, 279, 5302] "instructions of the states"
+#expected = [6722, 315, 28192, 5302] "capital of united states"
+
+
+
+g_tokenizer = AutoTokenizer.from_pretrained("google/gemma-3-1b-pt")
+g_model = AutoModelForCausalLM.from_pretrained("google/gemma-3-1b-pt")
+
+
+g_tokens = g_tokenizer.encode("the capital of united")
+#print(g_tokens)
+#input = [2, 1437, 5279, 529, 26974] the capital of united
+#expected = [1437, 5279, 529, 26974, 5022] the capital of united states
+#output = [184, 236743, 529,  506, 5022 ] <h1>  of the states
+
+g_out = g_model(torch.tensor([g_tokens]))
+#print(g_out.logits.shape)
+
+
+probs_gemma = torch.softmax(g_out.logits[0, 4, :], dim = -1)
+max_prob_gemma, max_index_gemma = torch.max(probs_gemma, dim = -1)
+
+#print(max_prob_gemma, max_index_gemma, probs_gemma)
+#print(g_tokenizer.decode([[1437, 5279, 529, 26974, 5022 ]]))
